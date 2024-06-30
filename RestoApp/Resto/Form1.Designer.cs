8c3a4671ee8d46dc06feb323cda409b0d3d177f7
packmansoft <restaurantpos485@gmail.com>
2024-06-30 12:58:24 +0300

    Add files via upload

---
+++ RestoApp/Resto/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Resto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Infragistics.Win.Appearance appearance1 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance2 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance3 = new Infragistics.Win.Appearance();
+            this.ultraButton5 = new Infragistics.Win.Misc.UltraButton();
+            this.SuspendLayout();
+            // 
+            // ultraButton5
+            // 
+            appearance1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            appearance1.BackColor2 = System.Drawing.Color.Gray;
+            appearance1.BackColorAlpha = Infragistics.Win.Alpha.UseAlphaLevel;
+            appearance1.BackColorDisabled = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            appearance1.BackGradientAlignment = Infragistics.Win.GradientAlignment.Element;
+            appearance1.BackGradientStyle = Infragistics.Win.GradientStyle.Vertical;
+            appearance1.BackHatchStyle = Infragistics.Win.BackHatchStyle.Horizontal;
+            this.ultraButton5.Appearance = appearance1;
+            this.ultraButton5.ButtonStyle = Infragistics.Win.UIElementButtonStyle.Button3D;
+            appearance2.BackColor = System.Drawing.Color.Green;
+            appearance2.BackColor2 = System.Drawing.Color.White;
+            this.ultraButton5.HotTrackAppearance = appearance2;
+            this.ultraButton5.Location = new System.Drawing.Point(361, 107);
+            this.ultraButton5.Name = "ultraButton5";
+            appearance3.BackColor = System.Drawing.Color.Red;
+            appearance3.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ultraButton5.PressedAppearance = appearance3;
+            this.ultraButton5.Size = new System.Drawing.Size(123, 61);
+            this.ultraButton5.TabIndex = 0;
+            this.ultraButton5.Text = "ultraButton1";
+            this.ultraButton5.UseAppStyling = false;
+            this.ultraButton5.UseOsThemes = Infragistics.Win.DefaultableBoolean.False;
+            this.ultraButton5.Paint += new System.Windows.Forms.PaintEventHandler(this.ultraButton5_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 410);
+            this.Controls.Add(this.ultraButton5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Infragistics.Win.Misc.UltraButton ultraButton5;
+    }
+}
+
---
+++ RestoApp/Resto/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Infragistics.Win;
+using Infragistics.Win.UltraWinEditors;
+using Infragistics.Win.Misc;
+using System.Drawing.Drawing2D;
+
+namespace Resto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomButton();
+        }
+        private UltraButton ultraButton2;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ultraButton1.UseOsThemes = Infragistics.Win.DefaultableBoolean.False;
+            //ultraButton1.ButtonStyle = Infragistics.Win.UIElementButtonStyle.FlatBorderless;
+
+
+        }
+        private void InitializeCustomButton()
+        {
+            this.ultraButton2 = new Infragistics.Win.Misc.UltraButton();
+            this.SuspendLayout();
+            // 
+            // ultraButton1
+            // 
+            this.ultraButton2.Location = new System.Drawing.Point(50, 50);
+            this.ultraButton2.Name = "ultraButton1";
+            this.ultraButton2.Size = new System.Drawing.Size(200, 50);
+            this.ultraButton2.TabIndex = 0;
+            this.ultraButton2.Text = "Round Corner Button";
+            this.ultraButton2.Paint += new PaintEventHandler(this.ultraButton1_Paint);
+            // 
+            // RoundCornerButtonForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 150);
+            this.Controls.Add(this.ultraButton2);
+            this.Name = "RoundCornerButtonForm";
+            this.Text = "Round Corner UltraButton";
+            this.ResumeLayout(false);
+        }
+
+        private void ultraButton1_Paint(object sender, PaintEventArgs e)
+        {
+            UltraButton button = sender as UltraButton;
+            Graphics g = e.Graphics;
+
+            // Set the smoothing mode to anti-alias for smooth edges
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Define the rectangle area for the button
+            Rectangle rect = new Rectangle(0, 0, button.Width, button.Height);
+
+            // Define the radius for the round corners
+            int cornerRadius = 20;
+
+            // Create a GraphicsPath to define the round corners
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddArc(rect.X, rect.Y, cornerRadius, cornerRadius, 180, 90);
+                path.AddArc(rect.X + rect.Width - cornerRadius, rect.Y, cornerRadius, cornerRadius, 270, 90);
+                path.AddArc(rect.X + rect.Width - cornerRadius, rect.Y + rect.Height - cornerRadius, cornerRadius, cornerRadius, 0, 90);
+                path.AddArc(rect.X, rect.Y + rect.Height - cornerRadius, cornerRadius, cornerRadius, 90, 90);
+                path.CloseFigure();
+
+                // Fill the button background
+                using (Brush brush = new SolidBrush(button.Appearance.BackColor))
+                {
+                    g.FillPath(brush, path);
+                }
+
+                // Draw the button border
+                using (Pen pen = new Pen(button.Appearance.BorderColor))
+                {
+                    g.DrawPath(pen, path);
+                }
+            }
+
+            // Draw the button text
+            TextRenderer.DrawText(g, button.Text, button.Font, rect, button.Appearance.ForeColor, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+        }
+
+        private void ultraButton5_Paint(object sender, PaintEventArgs e)
+        {
+            
+            UltraButton button = sender as UltraButton;
+            if (button == null)
+                return;
+
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Rectangle rect = new Rectangle(0, 0, button.Width, button.Height);
+            int radius = 20; // Adjust the radius for round corners
+
+            // Draw the button with rounded corners
+            using (GraphicsPath path = GetRoundedRectanglePath(rect, radius))
+            using (SolidBrush brush = new SolidBrush(button.BackColor))
+            using (Pen pen = new Pen(button.ForeColor, 1))
+            {
+                e.Graphics.FillPath(brush, path);
+                e.Graphics.DrawPath(pen, path);
+            }
+
+            // Draw the text
+            TextRenderer.DrawText(e.Graphics, button.Text, button.Font, rect, button.ForeColor, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+        
+    }
+        private GraphicsPath GetRoundedRectanglePath(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            int diameter = radius * 2;
+            path.AddArc(rect.X, rect.Y, diameter, diameter, 180, 90);
+            path.AddArc(rect.Right - diameter, rect.Y, diameter, diameter, 270, 90);
+            path.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - diameter, diameter, diameter, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+    }
+}
